6b81258efa5c5e055d1fdc3dfbceb21a87e536bc
MarkXtenda <mark.i2p.1998@gmail.com>
2023-01-20 15:52:44 +1100

    3. Migrating User and Post models

--- db/migrate/20230120043142_create_posts.rb
+++ db/migrate/20230120043142_create_posts.rb
@@ -1,6 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
+      t.belongs_to :user
       t.string "image"
       t.string "caption"
       t.integer "likes"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,6 @@
+User.create(username: "Luke")
+User.create(username: "Puke")
+User.create(username: "Muke")
 
-
+4.times{ |i| Post.create(user_id: 1, caption: "Post number:#{i}")
+}
